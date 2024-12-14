b7e5b0f9010811c47fcfb308ab0f4464b05b5944
marcebenitez2 <marcebenitez0607@gmail.com>
2024-12-14 12:17:39 -0300

    Correccion logger

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -1,51 +1,22 @@
 import winston from 'winston';
-import path from 'path';
-
-const isProduction = process.env.NODE_ENV === 'production';
-
-const transports = [];
-
-if (isProduction) {
-  transports.push(
-    new winston.transports.Console({
-      format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.colorize(),
-        winston.format.printf(({ timestamp, level, message, stack }) => {
-          return `${timestamp} [${level}]: ${message} ${stack ? `\n${stack}` : ''}`;
-        })
-      ),
-    })
-  );
-} else {
-  transports.push(
-    new winston.transports.File({
-      filename: path.join('logs', 'error.log'),
-      level: 'error',
-    }),
-    new winston.transports.File({
-      filename: path.join('logs', 'combined.log'),
-    }),
-    new winston.transports.Console({
-      format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.colorize(),
-        winston.format.printf(({ timestamp, level, message }) => {
-          return `${timestamp} [${level}]: ${message}`;
-        })
-      ),
-    })
-  );
-}
 
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.combine(
     winston.format.timestamp(),
     winston.format.errors({ stack: true }),
-    winston.format.json()
+    winston.format.printf(({ timestamp, level, message, stack }) => {
+      return `${timestamp} [${level}]: ${message} ${stack ? `\n${stack}` : ''}`;
+    })
   ),
-  transports,
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.colorize()
+      )
+    })
+  ]
 });
 
 export default logger;
